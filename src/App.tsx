0c18ab355d52e45ff948e4206f735751fb4f6ffb
tasuku1496 <tishiguro@careerindex.co.jp>
2024-09-03 09:28:22 +0900

    feat: Rowコンポーネントの呼び出しを記載

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,18 @@ import { Row } from "./components/Row";
 
 function App() {
   return (
-    // 追加箇所
     <div className="App">
-      <Row fetchUrl={requests.fetchNetflixOriginals} title={"タイトル"} />
+      <Row
+        title="NETFLIX ORIGUINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="News Movies" fetchUrl={requests.fetchNewsMovies} />
+      <Row title="Kids Movies" fetchUrl={requests.fetchKidsMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentMovies} />
     </div>
   );
 }
